676bfe039611e0c08e572f3fa7efa92b7da342b0
Hitory <hitory@yahoo.com>
2022-08-01 16:09:41 +0300

    Глава № 9.

---
+++ src/main/java/Exception_Handing/Exc_15/CustomExceptDemo.java
@@ -0,0 +1,28 @@
+package Exception_Handing.Exc_15;
+
+public class CustomExceptDemo {
+    public static void main(String[] args) {
+        //  В массиве numer содержатся нечётные числа
+        int numer[] = {4, 8, 15, 32, 64, 127, 256, 512};
+        int denom[] = {2, 0, 4, 4, 0, 8};
+        for (int i = 0; i < numer.length; i++) {
+            try {
+                if ((numer[i] % 2) != 0) {
+                    throw new NonResultException(numer[i], denom[i]);
+                }
+                System.out.println(numer[i] + " / " + denom[i] + " равно " + numer[i] / denom[i]);
+            }
+            catch (ArithmeticException exc) {
+                //  перехватить исключение
+                System.out.println("Попытка деления на нуль");
+            }
+            catch (ArrayIndexOutOfBoundsException exc) {
+                //  перехватить исключение
+                System.out.println("Соответствующий элемент не найден");
+            }
+            catch (NonResultException exc) {
+                System.out.println(exc);
+            }
+        }
+    }
+}
